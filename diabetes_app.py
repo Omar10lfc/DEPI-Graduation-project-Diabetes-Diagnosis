a9e438718a0435e10cfef92a94272989e9437c37
omar10lfc <omar.mashaly@ejust.edu.eg>
2024-10-23 18:33:35 +0300

    update

--- diabetes_app.py
+++ diabetes_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
-# Disable warnings
-st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
 
 @st.cache_data
 def load_data():
